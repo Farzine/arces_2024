a5d5c19ec230bef2ff05c4d2becbd8ca8a42112d
Amitsharma2468 <amitkmrsharma292@gmail.com>
2024-06-30 14:21:44 +0600

    update structure

---
+++ client/src/app/registration/page.tsx
@@ -0,0 +1,8 @@
+export default function Registration() {
+    return (
+      <main className="h-screen">
+        <h1 className="text-3xl font-semibold">Registration Page</h1>
+      </main>
+    );
+  }
+  
