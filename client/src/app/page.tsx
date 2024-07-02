02c6cba91f6aa306e56afb80eb30389577f531ef
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-07-03 00:49:52 +0600

    feat: Add Navbar component to Home Page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main className="h-screen">
+      <Navbar />
       <h1 className="text-3xl font-semibold">Home Page</h1>
+      
       <Footer />
     </main>
   );
