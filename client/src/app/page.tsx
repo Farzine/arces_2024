00328a824edd808ea73a343e04180fbf79f097c2
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-07-02 21:33:25 +0600

    feat: Add Footer components complete

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <main className="h-screen">
       <h1 className="text-3xl font-semibold">Home Page</h1>
+      <Footer />
     </main>
   );
 }
