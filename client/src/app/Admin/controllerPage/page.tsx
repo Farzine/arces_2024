f9b8c72863d4450ab74ef3a9bd7d00999cbfba3a
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-07-02 12:20:05 +0600

    Refactor AdminPage component layout and styles

--- client/src/app/Admin/controllerPage/page.tsx
+++ client/src/app/Admin/controllerPage/page.tsx
@@ -16,18 +16,20 @@ const AdminPage: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold mb-8 text-purple-700">ADMIN PAGE</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 mb-8 w-full max-w-4xl">
-        <div className="bg-white shadow-md rounded-lg p-2">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold text-blue-400">ADMIN PAGE</h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mb-4 w-full">
+        <div className="bg-white p-4">
           <NoticeCard />
         </div>
-        <div className="bg-white shadow-md rounded-lg p-2">
+        <div className="bg-white p-4">
           <ImportantDateCard />
         </div>
-        <div className="bg-white shadow-md rounded-lg p-2">
+        <div className="bg-white  p-4">
           <ResearchTrack />
         </div>
-        <div className="bg-white shadow-md rounded-lg p-2">
+        <div className="bg-white p-4">
           <IndustryTrack />
         </div>
       </div>
