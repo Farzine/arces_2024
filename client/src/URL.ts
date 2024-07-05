5ad3018a21a080c998e6391112dda15b0f6486b3
Amitsharma2468 <amitkmrsharma292@gmail.com>
2024-07-05 17:39:52 +0600

    registatration connected with backend

---
+++ client/src/URL.ts
@@ -0,0 +1,2 @@
+export const BACKENDURL = "http://localhost:5000/";
+export const FRONTENDURL = "http://localhost:3000/";
--- client/src/app/registration/page.tsx
+++ client/src/app/registration/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { BACKENDURL, FRONTENDURL } from "@/URL";
+import { useRouter } from "next/navigation";
 
 interface Country {
   name: string;
@@ -15,27 +19,35 @@ export default function ResearchTracks() {
     country: "",
     email: "",
     phone: "",
-    countryCode: "",
-    picture: null as File | null,
+    photoUrl: "",
   });
   const [countries, setCountries] = useState<Country[]>([]);
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const route = useRouter();
 
   useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all')
-      .then(response => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
         const countryData = response.data.map((country: any) => ({
           name: country.name.common,
           flag: country.flags.svg,
-          callingCode: country.idd.root + (country.idd.suffixes ? country.idd.suffixes[0] : ""),
+          callingCode:
+            country.idd.root +
+            (country.idd.suffixes ? country.idd.suffixes[0] : ""),
         }));
         // Sort the countries alphabetically by name
-        countryData.sort((a: Country, b: Country) => a.name.localeCompare(b.name));
+        countryData.sort((a: Country, b: Country) =>
+          a.name.localeCompare(b.name)
+        );
         setCountries(countryData);
       })
-      .catch(error => console.error('Error fetching countries:', error));
+      .catch((error) => console.error("Error fetching countries:", error));
   }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setForm({
       ...form,
@@ -43,25 +55,42 @@ export default function ResearchTracks() {
     });
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0] || null;
+  const handleProfilePicUpload = (result: any) => {
+    const uploadedURL = result.info.secure_url;
     setForm({
       ...form,
-      picture: file,
+      photoUrl: uploadedURL,
     });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
+    setSubmitting(true);
+    try {
+      const response = await axios.post(`${BACKENDURL}registration`, form);
+      if (response.status === 201) {
+        route.push(`${FRONTENDURL}attendee/${response.data._id}`);
+      }
+    } catch (error) {
+      window.alert("Can't Register User");
+    }
+    setSubmitting(false);
   };
 
   return (
     <main className="min-h-screen flex items-center justify-center p-4">
-      <form className="w-full max-w-lg p-8 bg-white shadow-md rounded-lg" onSubmit={handleSubmit}>
-        <h1 className="text-3xl font-semibold mb-8 text-center">Research Tracks Registration</h1>
+      <form
+        className="w-full max-w-lg p-8 bg-white shadow-md rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-3xl font-semibold mb-8 text-center">
+          Research Tracks Registration
+        </h1>
         <div className="mb-6">
-          <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
+          <label
+            htmlFor="name"
+            className="block text-gray-700 font-medium mb-2"
+          >
             Name
           </label>
           <input
@@ -75,7 +104,10 @@ export default function ResearchTracks() {
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="university" className="block text-gray-700 font-medium mb-2">
+          <label
+            htmlFor="university"
+            className="block text-gray-700 font-medium mb-2"
+          >
             University
           </label>
           <input
@@ -89,7 +121,10 @@ export default function ResearchTracks() {
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="country" className="block text-gray-700 font-medium mb-2">
+          <label
+            htmlFor="country"
+            className="block text-gray-700 font-medium mb-2"
+          >
             Country
           </label>
           <select
@@ -100,7 +135,9 @@ export default function ResearchTracks() {
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
             required
           >
-            <option value="" disabled>Select your country</option>
+            <option value="" disabled>
+              Select your country
+            </option>
             {countries.map((country) => (
               <option key={country.name} value={country.name}>
                 {country.name}
@@ -109,7 +146,10 @@ export default function ResearchTracks() {
           </select>
         </div>
         <div className="mb-6">
-          <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-medium mb-2"
+          >
             Email
           </label>
           <input
@@ -123,27 +163,10 @@ export default function ResearchTracks() {
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="countryCode" className="block text-gray-700 font-medium mb-2">
-            Country Code
-          </label>
-          <select
-            id="countryCode"
-            name="countryCode"
-            value={form.countryCode}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
-            required
+          <label
+            htmlFor="phone"
+            className="block text-gray-700 font-medium mb-2"
           >
-            <option value="" disabled>Select</option>
-            {countries.map((country) => (
-              <option key={country.callingCode} value={country.callingCode}>
-                {country.name} ({country.callingCode})
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-6">
-          <label htmlFor="phone" className="block text-gray-700 font-medium mb-2">
             Phone Number
           </label>
           <input
@@ -156,23 +179,32 @@ export default function ResearchTracks() {
             required
           />
         </div>
-        <div className="mb-6">
-          <label htmlFor="picture" className="block text-gray-700 font-medium mb-2">
-            Picture
-          </label>
-          <input
-            type="file"
-            id="picture"
-            name="picture"
-            accept="image/*"
-            onChange={handleFileChange}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
-            required
+        {form.photoUrl ? (
+          <Image
+            src={form.photoUrl}
+            height={200}
+            width={200}
+            alt=""
+            className="rounded-full h-32 w-32"
           />
-        </div>
+        ) : null}
+        <CldUploadWidget onUpload={handleProfilePicUpload} uploadPreset={process.env.NEXT_PUBLIC_IMG_UPLOAD_PRESET}>
+          {({ open }) => {
+            return (
+              <button
+                type="button"  // Changed type to "button" to prevent form submission
+                className="border my-2 p-2 rounded-md hover:bg-slate-300"
+                onClick={() => open()}
+              >
+                Upload your image <span className="text-red-600">*</span>
+              </button>
+            );
+          }}
+        </CldUploadWidget>
         <div className="text-center">
           <button
             type="submit"
+            disabled={submitting}
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
           >
             Submit
@@ -182,4 +214,3 @@ export default function ResearchTracks() {
     </main>
   );
 }
-
