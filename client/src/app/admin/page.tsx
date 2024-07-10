44bf18bcea5170dc97e985786d682fecc47e9312
Farzine <farzine07@student.sust.edu>
2024-07-10 23:06:18 +0600

    admin login page UI change

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
+import Image from "next/image";
+
 const baseUrl = process.env.NEXT_PUBLIC_APP_BACKEND_URL;
 
 export default function Login() {
@@ -44,11 +46,19 @@ export default function Login() {
   };
 
   return (
-    <div className="App">
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-xs">
+    <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-700 bg-opacity-50 z-50">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      )}
+      <div className={`flex w-full max-w-xl h-auto bg-white shadow-lg rounded-lg overflow-hidden ${isLoading ? 'blur-sm' : ''}`}>
+        <div className="relative w-1/2 h-auto">
+          <Image src="/sustGrafiti.jpg" alt="Mural" layout="fill" objectFit="cover" />
+        </div>
+        <div className="w-1/2 p-6 flex flex-col justify-between">
+          <h2 className="text-2xl font-bold mb-8 text-center">ADMIN LOGIN</h2>
           <form
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
             onSubmit={(e) => {
               e.preventDefault();
               handleLogin();
@@ -88,40 +98,33 @@ export default function Login() {
             </div>
             <div className="flex items-center justify-center">
               <button
-                className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex justify-center items-center"
+                className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline flex justify-center items-center"
                 type="submit"
                 disabled={isLoading}
               >
-                {isLoading ? (
-                  <svg
-                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                ) : (
-                  "Log in"
-                )}
+                Log in
               </button>
             </div>
             {error && (
               <p className="text-red-500 text-xs italic mt-4">{error}</p>
             )}
           </form>
+          <div className="flex justify-center mt-8">
+            <Image
+              src="/sustLogo.png"
+              alt="SUST Logo"
+              width={100}
+              height={100}
+              className="h-24 w-auto mx-2"
+            />
+            <Image
+              src="/icerieLogo.jpg"
+              alt="ICERIE logo"
+              width={100}
+              height={100}
+              className="h-24 w-auto mx-2"
+            />
+          </div>
         </div>
       </div>
     </div>
