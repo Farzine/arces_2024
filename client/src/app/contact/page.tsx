ca69dba8c6c423ccb95c0805239eec2b74c03a6a
Meraj2349 <109569196+Meraj2349@users.noreply.github.com>
2024-09-10 20:32:34 +0600

    Refactor font sizes in ContactUs page

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -23,13 +23,13 @@ const ContactUs: React.FC = () => {
             />
           </div>
           <div>
-          <h1 className="text-3xl font-bold mb-4 text-center">CONTACT US</h1>
+          <h1 className="text-5xl font-bold mb-4 text-center">CONTACT US</h1>
           </div>
         </div>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 mx-40">
           <div className="relative text-left mb-8 md:mb-0 pl-6 before:absolute before:left-0 before:top-0 before:bottom-0 before:w-1 before:bg-blue-500">
-            <h2 className="text-2xl font-bold text-red-600 mb-4">Conference Secretary</h2>
-            <p className='font-semibold text-lg mb-2'>Dr Md Tamej Uddin</p>
+            <h2 className="text-4xl font-bold text-red-600 mb-4">Conference Secretary</h2>
+            <p className='font-semibold text-xxl mb-2'>Dr Md Tamej Uddin</p>
             <p className='text-gray-600'>Department of Chemical Engineering and Polymer Science</p>
             <p>Shahjalal University of Science and Technology</p>
             <p>Sylhet, Bangladesh</p>
@@ -43,7 +43,7 @@ const ContactUs: React.FC = () => {
               height={25}
               className=" mr-2"
             />
-              <a href="mailto:icerie2025@sust.edu" className="text-blue-500 hover:underline text-lg">icerie2025@sust.edu</a>
+              <a href="mailto:icerie2025@sust.edu" className="text-blue-500 hover:underline text-xl">icerie2025@sust.edu</a>
             </p>
             <p className="flex items-center mb-4">
             <Image
@@ -53,7 +53,7 @@ const ContactUs: React.FC = () => {
               height={25}
               className=" mr-2"
             />
-              <span className='text-lg'>+8801700000000</span>
+              <span className='text-xl'>+8801700000000</span>
             </p>
             <p className="flex items-center mb-4">
             <Image
@@ -63,7 +63,7 @@ const ContactUs: React.FC = () => {
               height={25}
               className=" mr-2"
             />
-              <a href="https://www.facebook.com" className="text-blue-500 text-lg">Facebook</a>
+              <a href="https://www.facebook.com" className="text-blue-500 text-xl">Facebook</a>
             </p>
             <p className="flex items-center">
             <Image
@@ -73,7 +73,7 @@ const ContactUs: React.FC = () => {
               height={25}
               className=" mr-2"
             />
-              <span className='text-lg'>+8801800000000</span>
+              <span className='text-xl'>+8801800000000</span>
             </p>
           </div>
         </div>
