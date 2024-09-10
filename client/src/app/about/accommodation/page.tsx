4485e4af1569e583c680a7bd3d7c1bbacd212b29
Farzine <farzine07@student.sust.edu>
2024-09-11 04:34:26 +0600

    Refactor page layout and styling in Accommodation component

--- client/src/app/about/accommodation/page.tsx
+++ client/src/app/about/accommodation/page.tsx
@@ -12,18 +12,18 @@ export default function Accommodation() {
     <div className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
       </div>
-      <div  className="h-screen flex flex-col justify-center items-center bg-white">
+      <div  className="h-screen flex flex-col justify-center md:items-center bg-white px-16">
       
-      <h1 className="text-3xl font-semibold mb-4">Accommodation</h1>
-      <p className="text-lg text-gray-600 mb-6">
+      <h1 className="md:text-5xl text-4xl font-semibold mb-4">Accommodation</h1>
+      <p className="md:text-3xl text-2xl text-gray-600 mb-6">
         We will announce the accommodation details soon.
       </p>
-      <Link
+      {/* <Link
         href="/" // Update with the actual route you want to navigate to
         className="px-6 py-2 text-white bg-red-500 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
       >
         Continue
-      </Link>
+      </Link> */}
     </div>
     {/* Wrap the Carousel and Footer components inside a div */}
     <div>
