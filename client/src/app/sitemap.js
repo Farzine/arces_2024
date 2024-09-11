b1ff9b8f32586ba29c21db5df7e42aa2430f395a
Farzine <farzine07@student.sust.edu>
2024-09-11 23:27:02 +0600

    Add sitemap package and generate sitemap.xml
    - Added "sitemap" package to the client's package.json
    - Created a new file "sitemap.js" in the client's app directory
    - Implemented a function to generate a sitemap.xml file
    - Added static URLs and their metadata to the sitemap
    - Created a server-side function to serve the sitemap

---
+++ client/src/app/sitemap.js
@@ -0,0 +1,63 @@
+import { GetServerSideProps } from 'next';
+
+// Define the base URL of your site
+const baseUrl = 'https://icerie2025.sust.edu';
+
+// Define the static pages of your website
+const staticPages = [
+  '/',
+  '/tracks',
+  '/authors/submission',
+  '/authors/callForPaper',
+  '/authors/importantDates',
+  '/registration/registrationFees',
+  '/attendee',
+  '/schedule',
+  '/gallery',
+  '/about/icerie',
+  '/about/committee',
+  '/about/sponsors',
+  '/about/venue',
+  '/contact',
+];
+
+// Generate sitemap
+function generateSitemap() {
+  const date = new Date().toISOString();
+
+  const staticUrls = staticPages
+    .map((page) => {
+      return `
+        <url>
+          <loc>${baseUrl}${page}</loc>
+          <lastmod>${date}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.8</priority>
+        </url>
+      `;
+    })
+    .join('');
+
+  return `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticUrls}
+    </urlset>
+  `;
+}
+
+// Create a server-side function to serve the sitemap
+export const getServerSideProps = async ({ res }) => {
+  const sitemap = generateSitemap();
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default function Sitemap() {
+  return null;
+}
